6a7b0146cd1726ce14a24fa583a1a505a2af4486
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-16 19:12:59 +0530

    Create dml-pkl.sql

---
+++ dml-pkl.sql
@@ -0,0 +1,42 @@
+
+-- PROGRESSION 1:
+
+--1. Insert into city
+
+--2. Insert into referee
+
+--3. Insert into innings
+
+--4. Insert into extra_type
+
+--5. Insert into skill
+
+--6. Insert into team
+
+--7. Insert into player
+
+--8. Insert into venue
+
+--9. Insert into event
+
+--10. Insert into extra_event
+
+--11. Insert into outcome
+
+--12. Insert into game
+
+--13. Update player table
+
+--14. Update player table
+
+--15. Update player table
+
+--16. Update player table
+
+--17. Delete from extra_type
+
+--18. Delete from referee
+
+--19. Delete from player
+
+--20. Delete from outcome
